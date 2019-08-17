7a5d87d4012af7aa1d3c083991e5944354a5c0e5
Samuel Feese <samuelfeese@outlook.com>
2019-08-17 19:49:28 -0400

    this is a struggle

---
+++ Dummy Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dummy_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
